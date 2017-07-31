1566284adc2febd29d9c279310c603d035243923
jpmmaia <jpmmaia@gmail.com>
2017-07-31 16:54:30 +0100

    - Testing game objects.

---
+++ MythologyTestProject/GameEngine/GameObject/GameObjectTests.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "GameEngine/GameObject/GameObject.h"
+#include "GameEngine/Component/Lights/PointLightComponent.h"
+#include "Common/Timer.h"
+
+using namespace GameEngine;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MythologyTestProject
+{
+	TEST_CLASS(GameObjectTests)
+	{
+	public:
+		TEST_METHOD(GameObjectTest1)
+		{
+			using Vector3 = Eigen::Vector3f;
+			using Quaternion = Eigen::Quaternion<float>;
+
+			GameObject gameObject;
+
+			// Test default construction:
+			{
+				auto& transform = gameObject.Transform();
+				Assert::IsTrue(transform.Translation() == Vector3::Zero());
+				Assert::IsTrue(transform.Rotation().isApprox(Quaternion::Identity()));
+				Assert::IsTrue(transform.Scale() == Vector3(1.0f, 1.0f, 1.0f));
+			}
+
+			// Test components:
+			PointLightComponent pointLightComponent;
+			{
+				gameObject.AddComponent("PointLight", pointLightComponent);
+				auto& component = gameObject.GetComponent("PointLight");
+				Assert::IsTrue(&component == &pointLightComponent);
+
+				gameObject.RemoveComponent("PointLight");
+				Assert::IsFalse(gameObject.HasComponent("PointLight"));
+			}
+
+			// Test transform logic:
+			{
+				Common::Timer timer(std::chrono::milliseconds(12));
+
+				gameObject.AddComponent("PointLight", pointLightComponent);
+
+				auto& transform = gameObject.Transform();
+				transform.SetTranslation({1.0f, -2.0f, 5.0f});
+				transform.FixedUpdate(timer);
+				// TODO
+
+			}
+		}
+	};
+}
