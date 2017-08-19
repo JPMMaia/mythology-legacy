96ce2d9c8ac6226667ddcc7553a02061b1eacaf3
jpmmaia <jpmmaia@gmail.com>
2017-08-19 01:15:31 +0100

    - Adding SkinnedModelInstance

---
+++ Mythology/GameEngine/Component/Meshes/SkinnedModelInstance.cpp
@@ -0,0 +1,15 @@
+﻿#include "SkinnedModelInstance.h"
+
+using namespace GameEngine;
+
+void SkinnedModelInstance::FrameUpdate(const Common::Timer& timer)
+{
+	using milliseconds = std::chrono::duration<float, std::milli>;
+	
+	m_timePosition += milliseconds(timer.GetDeltaTime()).count();
+
+	if(m_timePosition > m_skinnedData.GetClipEndTime(m_currentAnimationClipName))
+		m_timePosition = 0.0f;
+
+	m_skinnedData.GetFinalTransforms(m_currentAnimationClipName, m_timePosition, m_finalTransforms);
+}
---
+++ Mythology/GameEngine/Component/Meshes/SkinnedModelInstance.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "GameEngine/Geometry/Animation/SkinnedData.h"
+#include "Common/Timer.h"
+#include "GameEngine/Geometry/EigenGeometry.h"
+#include "GameEngine/Geometry/Primitives/CustomGeometry.h"
+#include "MeshComponent.h"
+
+#include <memory>
+
+namespace GameEngine
+{
+	class SkinnedModelInstance
+	{
+	public:
+
+	public:
+		void FrameUpdate(const Common::Timer& timer);
+
+	private:
+		GameEngine::SkinnedData m_skinnedData;
+		std::vector<Eigen::Affine3f> m_finalTransforms;
+		std::string m_currentAnimationClipName;
+		float m_timePosition = 0.0f;
+	};
+}
