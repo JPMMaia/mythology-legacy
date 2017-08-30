4ffa4b5fdc6b3d301f76f6a5f36ca6cd33cad4ce
jpmmaia <jpmmaia@gmail.com>
2017-08-30 11:24:10 +0100

    - Adding required dll for the app to start correctly.

--- Mythology/GameEngine/Physics/PhysicsManager.cpp
+++ Mythology/GameEngine/Physics/PhysicsManager.cpp
@@ -20,9 +20,9 @@ PhysicsManager::PhysicsManager()
 		throw std::runtime_error("PxCreateFoundation failed!");
 
 	// Create physics visual debugger:
-	/*m_physicsVisualDebugger = Pointer<PxPvd>(PxCreatePvd(*m_foundation));
+	m_physicsVisualDebugger = Pointer<PxPvd>(PxCreatePvd(*m_foundation));
 	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-	m_physicsVisualDebugger->connect(*transport, PxPvdInstrumentationFlag::eALL);*/
+	m_physicsVisualDebugger->connect(*transport, PxPvdInstrumentationFlag::eALL);
 
 	// Create physics object:
 	auto recordMemoryAllocations = true;
