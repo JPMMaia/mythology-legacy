3687b4159af43cc44533cf7bffde1326bc2de53a
jpmmaia <jpmmaia@gmail.com>
2017-09-16 14:39:22 +0100

    - Adding metallicness and roughness to the GBuffer.

--- UniversalWindows/DirectX12Engine/Shaders/Standard/LightingPassPixelShader.hlsl
+++ UniversalWindows/DirectX12Engine/Shaders/Standard/LightingPassPixelShader.hlsl
@@ -9,19 +9,28 @@ struct PixelInput
 
 ConstantBuffer<PassData> g_passData : register(b0);
 Texture2D g_positions : register(t0, space0);
-Texture2D g_albedo : register(t1, space0);
-Texture2D g_normals : register(t2, space0);
+Texture2D g_baseColorAndMetallicness : register(t1, space0);
+Texture2D g_normalsAndRoughness : register(t2, space0);
 
 float4 main(PixelInput input) : SV_TARGET
 {
-	float4 albedo = g_albedo.Sample(g_samplerPointClamp, input.TextureCoordinates);
+	// Sample position:
 	float3 positionW = g_positions.Sample(g_samplerPointClamp, input.TextureCoordinates).xyz;
-	float3 normalW = g_normals.Sample(g_samplerPointClamp, input.TextureCoordinates).xyz;
+
+	// Sample base color and metallicness:
+	float4 baseColorAndMetallicness = g_baseColorAndMetallicness.Sample(g_samplerPointClamp, input.TextureCoordinates);
+	float3 baseColor = baseColorAndMetallicness.rgb;
+	float metallicness = baseColorAndMetallicness.a; // TODO check gamma correction
+
+	// Sample normal and roughness:
+	float4 normalAndRoughness = g_normalsAndRoughness.Sample(g_samplerPointClamp, input.TextureCoordinates);
+	float3 normalW = normalize(normalAndRoughness.xyz);
+	float roughness = normalAndRoughness.w;
 
 	Material material;
-	material.DiffuseAlbedo = albedo;
+	material.DiffuseAlbedo = float4(baseColor, 1.0f);
 	material.FresnelR0 = float3(0.1f, 0.1f, 0.1f);
-	material.Shininess = 1.0f;
+	material.Shininess = 1.0f - roughness;
 
 	float3 toEyeDirection = normalize(g_passData.CameraPositionW - positionW);
 	float shadowFactor = 1.0f;
