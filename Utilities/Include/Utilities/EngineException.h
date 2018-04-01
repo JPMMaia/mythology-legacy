1820bb2c2bcc3c6f48e399f036b1d0e0940ee8bd
jpmmaia <jpmmaia@gmail.com>
2018-04-01 17:43:29 +0300

    Initial draft for cmake

---
+++ Utilities/Include/Utilities/EngineException.h
@@ -0,0 +1,24 @@
+﻿#ifndef MAIA_UTILITIES_ENGINEEXCEPTION_H_INCLUDED
+#define MAIA_UTILITIES_ENGINEEXCEPTION_H_INCLUDED
+
+#include <string>
+
+namespace Maia
+{
+	namespace Utilities
+	{
+		class EngineException : public std::exception
+		{
+		public:
+			EngineException() = default;
+			explicit EngineException(const std::string& message);
+
+			const char* what() const noexcept override;
+
+		private:
+			std::string m_message;
+		};
+	}
+}
+
+#endif
---
+++ Utilities/Source/EngineException.cpp
@@ -0,0 +1,27 @@
+﻿export module Utilities.Exceptions;
+
+import std.core;
+
+namespace Maia
+{
+	namespace Utilities
+	{
+		class EngineException : public std::exception
+		{
+		public:
+			EngineException() = default;
+			explicit EngineException(const std::string& message) :
+				m_message(message)
+			{
+			}
+
+			const char* what() const noexcept override
+			{
+				return m_message.c_str();
+			}
+
+		private:
+			std::string m_message;
+		};
+	}
+}
